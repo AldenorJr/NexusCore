ef1e61896b6d63d0a302cfe5d01bad37bfc6ccac
AldenorJr <71083804+AldenorJr@users.noreply.github.com>
2023-10-02 11:53:29 -0300

    Updating

--- src/main/java/br/com/nexus/main/core/util/ConfigurationFile.java
+++ src/main/java/br/com/nexus/main/core/util/ConfigurationFile.java
@@ -13,11 +13,9 @@ import static org.apache.commons.io.FileUtils.getFile;
 public class ConfigurationFile {
 
     private final Bungeecord bungeecord;
-
     public ConfigurationFile(Bungeecord bungeecord) {
         this.bungeecord = bungeecord;
     }
-
     public void loadConfig() throws IOException {
         File file = getFile();
         if(!hasFile(file)) createFile(file);
@@ -31,42 +29,33 @@ public class ConfigurationFile {
         if(!configuration.contains("MongoDB")) setDefaultMongoDB(configuration);
         if(!configuration.contains("Token")) setDefaultToken(configuration);
     }
-
     private void setDefaultToken(Configuration configuration) {
         configuration.set("Token", "ASIJDAASIDU43UASSDJASOPA@!#@ODSB");
     }
-
     private void setDefaultRedis(Configuration configuration) {
         configuration.set("Redis.address", "127.0.0.1");
         configuration.set("Redis.port", 6379);
     }
-
     private void setDefaultMongoDB(Configuration configuration) {
         configuration.set("MongoDB.address", "127.0.0.1");
         configuration.set("MongoDB.port", 27017);
     }
-
     public Configuration getConfig() throws IOException {
         return ConfigurationProvider.getProvider(YamlConfiguration.class).load(getFile());
     }
-
     public void saveConfig(Configuration configuration) throws IOException {
         ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, getFile());
     }
-
     private void saveConfig(File file, Configuration configuration) throws IOException {
         ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, file);
     }
-
     private File getFile() {
         if(!hasDataFolder()) createDataFolder();
         return new File(bungeecord.getDataFolder(), "configuration.yml");
     }
-
     private boolean hasDataFolder() {
         return bungeecord.getDataFolder().exists();
     }
-
     private void createDataFolder() {
         bungeecord.getDataFolder().mkdir();
     }
